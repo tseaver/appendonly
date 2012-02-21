060825980b30e8392f96aae486ffa51418ea89c3
Tres Seaver <tseaver@palladion.com>
2012-02-21 21:54:56 +0000

    Prep 0.10 release.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(name='appendonly',
-      version='0.10dev',
+      version='0.10',
       description='Persistent append-only data structures.',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
